44ecc9c9063719490e44257057f71272154b183d
Damien Seguin <dmnsgn@gmail.com>
2015-07-16 14:55:44 +0100

    Fix nothing case

--- gulp/init/index.js
+++ gulp/init/index.js
@@ -1,4 +1,7 @@
-import exec from 'child_process'
+import {
+  exec
+}
+from 'child_process'
 import fs from 'fs'
 
 import chalk from 'chalk'
@@ -9,26 +12,28 @@ import choices from './choices.js'
 let questions = [{
   type: "input",
   name: "app_name",
-  message: "What's your app name?",
+  message: "App name",
   default: 'App'
 }, {
   type: "list",
   name: "language",
-  message: "What script compiler/transpiler do you want to use?",
+  message: "Script compiler/transpiler",
   choices: choices(__dirname + '/language/')
 }, {
   type: "list",
   name: "framework",
-  message: "What js framework do you want to use?",
+  message: "JS framework",
   choices: choices(__dirname + '/framework/')
 }, {
   type: "list",
   name: "preprocessor",
-  message: "What css preprocessor do you want to use?",
+  message: "CSS preprocessor",
   choices: choices(__dirname + '/preprocessor/')
 }];
 
 inquirer.prompt(questions, function(data) {
+
+  console.log(data);
 
   let transform = [].concat(data.language.transform, data.framework.transform);
   let extensions = {
@@ -39,8 +44,12 @@ inquirer.prompt(questions, function(data) {
     return t;
   }), extensions);
 
-  let dependencies = [].concat(data.language.dependencies, data.framework.dependencies, data.preprocessor.dependencies);
-  let devDependencies = [].concat(data.language.devDependencies, data.framework.devDependencies, data.preprocessor.devDependencies);
+  let dependencies = [].concat(data.language.dependencies, data.framework.dependencies, data.preprocessor.dependencies).filter(function(n) {
+    return n != undefined;
+  });
+  let devDependencies = [].concat(data.language.devDependencies, data.framework.devDependencies, data.preprocessor.devDependencies).filter(function(n) {
+    return n != undefined;
+  });
   updateDependencies(dependencies, devDependencies);
 
 });
@@ -114,13 +123,12 @@ function updateDependencies(dependencies, devDependencies) {
   let command = '';
 
   if (dependencies.join('') !== '') {
-    command += 'npm install --save' + dependencies.join(' ');
+    command += 'npm install --save' + dependencies.join(' ') + ' && ';
   }
   if (devDependencies.join('') !== '') {
-    command += ' && npm install --save-dev ' + devDependencies.join(' ');
+    command += 'npm install --save-dev ' + devDependencies.join(' ') + ' && ';
   }
-  command += ' && npm install';
-
+  command += 'npm install';
   if (dependencies || devDependencies) {
     console.log(chalk.green('Installing dependencies and adding them to package.json...', dependencies.join(' '), devDependencies.join(' ')));
     exec(command, function (error, stdout, stderr) {
