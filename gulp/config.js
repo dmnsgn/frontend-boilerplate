e5f7b9fa617389b3779fd5dd749a0829658e931a
Damien SEGUIN <dmnsgn@gmail.com>
2015-07-19 20:18:08 +0100

    Use config and directories from package.json

--- gulp/config.js
+++ gulp/config.js
@@ -5,10 +5,6 @@
 import pkg from '../package.json';
 import minimist from 'minimist';
 
-const SOURCE = 'src';
-const DIST = 'dist';
-const TEST = 'test';
-
 // Options
 let options = {
   string: 'env',
@@ -19,14 +15,6 @@ let options = {
 
 export default {
   args: minimist(process.argv.slice(2), options),
-  verbose: false,
-  port: 3000,
-  src: SOURCE,
-  dist: DIST,
-  test: TEST,
-  browsers: ['last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
-  prodUrl: 'http://test.tld',
-  analyticsUA: 'UA-XXXXX-X',
   banner: ['/**',
     ' * ' + pkg.title,
     ' * ' + pkg.description,
