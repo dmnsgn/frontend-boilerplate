fb900514a92c2b971732d05aca1d9bd4b8947448
Damien SEGUIN <dmnsgn@gmail.com>
2015-09-06 13:24:24 +0100

    Rename boilerplate folder

--- boilerplate/tasks/index.js
+++ boilerplate/tasks/index.js
@@ -5,14 +5,16 @@
 import fs from 'fs';
 
 import gulp from 'gulp';
-import cfg from './config';
-import pkg from '../package.json';
+import cfg from '../config';
+import pkg from '../../package.json';
 
 global.gulp = gulp;
 global.config = Object.assign(cfg, pkg.config, pkg.directories);
 global.pkg = pkg;
 
-let tasks = fs.readdirSync('./gulp/tasks/');
+let tasks = fs.readdirSync('./boilerplate/tasks/');
 tasks.forEach(function(task) {
-	require(`./tasks/${task}`);
+  if (task !== 'index') {
+    require(`./${task}`);
+  }
 });
