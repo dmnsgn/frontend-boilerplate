5082968366d1f7f4d64a891648180a784fe5ed8b
Damien SEGUIN <dmnsgn@gmail.com>
2015-12-02 23:30:40 +0000

    Create static folder + Update clean task

--- boilerplate/tasks/clean.js
+++ boilerplate/tasks/clean.js
@@ -6,7 +6,7 @@ import cache from 'gulp-cache';
 import config from '../config';
 
 export function cleanDeleteFiles(done) {
-  return del([`${config.dist}/*`, '.sass-cache'], done);
+  return del([`${config.dist}/**`, `!${config.dist}`,`!${config.dist}/static`, '.sass-cache'], done);
 }
 
 export function cleanClearCache(done) {
