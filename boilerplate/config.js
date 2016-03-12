c1a98cafb46fbba06c4ed64b6e5d6e6c5401f24b
Damien Seguin <dmnsgn@gmail.com>
2016-03-12 14:56:37 +0000

    Update vendor when changes

--- boilerplate/config.js
+++ boilerplate/config.js
@@ -1,5 +1,7 @@
-import pkg from '../package.json';
+import * as fs from 'fs';
 import minimist from 'minimist';
+
+let config;
 
 const options = {
   string: 'env',
@@ -8,20 +10,23 @@ const options = {
   }
 };
 
-const banner =
-`/**
- * ${pkg.title}
- * ${pkg.description}
- * Compiled: ${Date()}
- * @version v${pkg.version}
- * @link ${pkg.homepage}
- * @copyright ${pkg.license}
- */
-`;
+export function updateConfig(done) {
+  const pkg = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf-8' }));
 
-export default Object.assign({
+  const banner =
+  `/**
+   * ${pkg.title}
+   * ${pkg.description}
+   * Compiled: ${Date()}
+   * @version v${pkg.version}
+   * @link ${pkg.homepage}
+   * @copyright ${pkg.license}
+   */
+  `;
+
+  config = Object.assign({
     args: minimist(process.argv.slice(2), options),
-    banner: banner
+    banner
   }, {
     version: pkg.version,
     title: pkg.title,
@@ -29,6 +34,17 @@ export default Object.assign({
     author: pkg.author,
     extensions: pkg.extensions,
     vendors: pkg.vendors
-  },
-  pkg.config,
-  pkg.directories);
+  }, pkg.config, pkg.directories);
+
+  done();
+}
+
+updateConfig(() => {});
+
+export default config;
+
+export function getConfig() {
+  updateConfig(() => {});
+
+  return config;
+}
