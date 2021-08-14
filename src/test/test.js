7472f0a852e34c33379a5b122bb0638538b9579f
Damien Seguin <dmnsgn@gmail.com>
2021-08-14 16:14:17 +0100

    test: update test files

--- src/test/test.js
+++ src/test/test.js
@@ -1,18 +1,34 @@
-import "./test.css";
+import css from "./test.css";
+import ejs from "./test.ejs";
 import glsl from "./test.glsl";
+import html from "./test.html";
 import img from "./test.png";
-
-import "./test.svg?sprite";
+import less from "./test.less";
+import md from "./test.md";
+import scss from "./test.scss";
+import styl from "./test.styl";
 import svg from "./test.svg";
+import svgSource from "./test.svg?source";
+import svgSprite from "./test.svg?sprite";
+import xml from "./test.xml";
 import Component from "./test.ts";
 
-console.log(glsl);
-console.log(img);
-console.log(svg);
+console.log("css", css);
+console.log("ejs", ejs({ data: "data" }));
+console.log("glsl", glsl);
+console.log("html", html);
+console.log("img", img);
+console.log("less", less);
+console.log("md", md);
+console.log("scss", scss);
+console.log("styl", styl);
+console.log("svg", svg);
+console.log("svgSource", svgSource);
+console.log("svgSprite", svgSprite);
+console.log("xml", xml);
 
 const svgImage = document.createElement("img");
 svgImage.src = svg;
-
 document.body.appendChild(svgImage);
 
 // typescript
@@ -22,14 +38,15 @@ component.init();
 // syntax-dynamic-import
 async function getComponent() {
   var element = document.createElement("div");
-  const importLodash = await import(/* webpackChunkName: "lodash" */ "lodash");
-  const _ = importLodash.default();
-
+  const { default: _ } = await import(
+    /* webpackChunkName: "lodash" */ "lodash"
+  );
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   return element;
 }
 
 getComponent().then((component) => {
+  console.log(component);
   document.body.appendChild(component);
 });
 
---
+++ src/test/test.md
@@ -0,0 +1,9 @@
+# test markdown
+
+[test](/test.png)
+
+<img src="./test.png" alt="">
+
+<svg>
+	<use xlink:href="/sprite.svg#test--sprite"></use>
+</svg>
